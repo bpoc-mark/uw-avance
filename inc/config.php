4d359ac1b69217e243f4f5178ad28f394c397515
bpoc-mark <markariel.maata@gmail.com>
2022-01-07 14:57:31 +0800

    added header pc and sp view

--- inc/config.php
+++ inc/config.php
@@ -16,13 +16,16 @@ define("CANONICAL", htmlspecialchars($PROTOCOL . $_SERVER['SERVER_NAME'] . $_SER
 
 
 //サイト名
-define("SITE_NAME", "");
+define("SITE_NAME", "株式会社アバンセシステム -安否確認システム 静岡のシステム開発 アプリケーション開発																										
+");
 
 //キーワード
-define("KEY_WORD", "");
+define("KEY_WORD", "静岡 システム開発,ソフトウェア開発,アプリケーション開発,アプリ開発,安否確認システム,ANPIC防災,BCP対策,パンデミック対策																										
+");
 
 //ディスクリプション
-define("DESCRIPTION", "");
+define("DESCRIPTION", "アバンセシステムでは、楽器・光技術・通信・輸送機器の各分野でアプリケーションソフト、ミドルウェア、ファームウェア（組み込み）対応の他、安否確認システム「ANPIC（アンピック）」のご提供と、幅広い業界のソフトウェア開発のニーズにお応えいたします。																										
+");
 
 //H1文言
 define("SEO_WORDS", "");
