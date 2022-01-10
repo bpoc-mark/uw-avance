4a2b902694a969ae1af08e3863c3ec0e7accb021
bpoc-mark <markariel.maata@gmail.com>
2022-01-10 14:06:33 +0800

    update withd to fixed witdh / update transition movement

--- inc/footer_box.php
+++ inc/footer_box.php
@@ -1,126 +1,126 @@
 <div class="footer__inner">
+	<div class="container">
+		<div class="logo"><a href="/"><img src="/images/common/logo_w.png" alt="<?php echo SITE_NAME; ?>"></a></div>
 
-	<div class="logo"><a href="/"><img src="/images/common/logo_w.png" alt="<?php echo SITE_NAME; ?>"></a></div>
-
-	<nav class="footer_nav">
-		<ul class="nav">
-			<li class="nav--item">
-				<a href="/" class="nav--link">HOME</a>
-			</li>
-			<li class="nav--item">
-				<a href="/" class="nav--link">事業内容</a>
-				<ul class="nav--item__submenu">
-					<li class="nav--item__submenu--item">
-						<a href="" class="nav--item__submenu--link">車載関連</a>
-					</li>
-					<li class="nav--item__submenu--item">
-						<a href="" class="nav--item__submenu--link">IoT関連</a>
-					</li>
-					<li class="nav--item__submenu--item">
-						<a href="" class="nav--item__submenu--link">生産管理</a>
-					</li>
-					<li class="nav--item__submenu--item">
-						<a href="" class="nav--item__submenu--link">大学教育関連</a>
-					</li>
-					<li class="nav--item__submenu--item">
-						<a href="" class="nav--item__submenu--link">安否確認</a>
-					</li>
-				</ul>
-			</li>
-			<li class="nav--item">
-				<a href="/" class="nav--link">会社案内</a>
-				<ul class="nav--item__submenu">
-					<li class="nav--item__submenu--item">
-						<a href="" class="nav--item__submenu--link">トップメッセージ</a>
-					</li>
-					<li class="nav--item__submenu--item">
-						<a href="" class="nav--item__submenu--link">事業内容</a>
-					</li>
-					<li class="nav--item__submenu--item">
-						<a href="" class="nav--item__submenu--link">開発環境実績</a>
-					</li>
-					<li class="nav--item__submenu--item">
-						<a href="" class="nav--item__submenu--link">沿革</a>
-					</li>
-				</ul>
-			</li>
-			<li class="nav--item">
-				<a href="/" class="nav--link">採用情報</a>
-				<nav class="submenu-wrap">
+		<nav class="footer_nav">
+			<ul class="nav">
+				<li class="nav--item">
+					<a href="/" class="nav--link">HOME</a>
+				</li>
+				<li class="nav--item">
+					<a href="/" class="nav--link">事業内容</a>
 					<ul class="nav--item__submenu">
 						<li class="nav--item__submenu--item">
-							<a href="" class="nav--item__submenu--link">人への想い</a>
+							<a href="" class="nav--item__submenu--link">車載関連</a>
 						</li>
 						<li class="nav--item__submenu--item">
-							<a href="" class="nav--item__submenu--link">仕事案内</a>
+							<a href="" class="nav--item__submenu--link">IoT関連</a>
 						</li>
 						<li class="nav--item__submenu--item">
-							<a href="" class="nav--item__submenu--link">会社見学</a>
+							<a href="" class="nav--item__submenu--link">生産管理</a>
 						</li>
 						<li class="nav--item__submenu--item">
-							<a href="" class="nav--item__submenu--link">募集要項</a>
+							<a href="" class="nav--item__submenu--link">大学教育関連</a>
 						</li>
 						<li class="nav--item__submenu--item">
-							<a href="" class="nav--item__submenu--link">エントリーフォーム</a>
+							<a href="" class="nav--item__submenu--link">安否確認</a>
 						</li>
 					</ul>
+				</li>
+				<li class="nav--item">
+					<a href="/" class="nav--link">会社案内</a>
 					<ul class="nav--item__submenu">
 						<li class="nav--item__submenu--item">
-							<a href="" class="nav--item__submenu--link">先輩インタビュー</a>
+							<a href="" class="nav--item__submenu--link">トップメッセージ</a>
 						</li>
 						<li class="nav--item__submenu--item">
-							<a href="" class="nav--item__submenu--link">入社3年目 ○○事業部</a>
+							<a href="" class="nav--item__submenu--link">事業内容</a>
 						</li>
 						<li class="nav--item__submenu--item">
-							<a href="" class="nav--item__submenu--link">入社3年目 ○○事業部</a>
+							<a href="" class="nav--item__submenu--link">開発環境実績</a>
 						</li>
 						<li class="nav--item__submenu--item">
-							<a href="" class="nav--item__submenu--link">入社3年目 ○○事業部</a>
-						</li>
-						<li class="nav--item__submenu--item">
-							<a href="" class="nav--item__submenu--link">入社3年目 ○○事業部</a>
+							<a href="" class="nav--item__submenu--link">沿革</a>
 						</li>
 					</ul>
-				</nav>
-			</li>
-			<li class="nav--item">
-				<a href="/" class="nav--link">アバンセラボ</a>
-			</li>
-			<li class="nav--item">
-				<a href="/" class="nav--link">新着情報</a>
-			</li>
-			<li class="nav--item">
-				<a href="/" class="nav--link">お問い合わせ</a>
-			</li>
-			<li class="nav--item">
-				<a href="/" class="nav--link">プライバシーポリシー</a>
-			</li>
-		</ul>
-	</nav>
+				</li>
+				<li class="nav--item">
+					<a href="/" class="nav--link">採用情報</a>
+					<nav class="submenu-wrap">
+						<ul class="nav--item__submenu">
+							<li class="nav--item__submenu--item">
+								<a href="" class="nav--item__submenu--link">人への想い</a>
+							</li>
+							<li class="nav--item__submenu--item">
+								<a href="" class="nav--item__submenu--link">仕事案内</a>
+							</li>
+							<li class="nav--item__submenu--item">
+								<a href="" class="nav--item__submenu--link">会社見学</a>
+							</li>
+							<li class="nav--item__submenu--item">
+								<a href="" class="nav--item__submenu--link">募集要項</a>
+							</li>
+							<li class="nav--item__submenu--item">
+								<a href="" class="nav--item__submenu--link">エントリーフォーム</a>
+							</li>
+						</ul>
+						<ul class="nav--item__submenu">
+							<li class="nav--item__submenu--item">
+								<a href="" class="nav--item__submenu--link">先輩インタビュー</a>
+							</li>
+							<li class="nav--item__submenu--item">
+								<a href="" class="nav--item__submenu--link">入社3年目 ○○事業部</a>
+							</li>
+							<li class="nav--item__submenu--item">
+								<a href="" class="nav--item__submenu--link">入社3年目 ○○事業部</a>
+							</li>
+							<li class="nav--item__submenu--item">
+								<a href="" class="nav--item__submenu--link">入社3年目 ○○事業部</a>
+							</li>
+							<li class="nav--item__submenu--item">
+								<a href="" class="nav--item__submenu--link">入社3年目 ○○事業部</a>
+							</li>
+						</ul>
+					</nav>
+				</li>
+				<li class="nav--item">
+					<a href="/" class="nav--link">アバンセラボ</a>
+				</li>
+				<li class="nav--item">
+					<a href="/" class="nav--link">新着情報</a>
+				</li>
+				<li class="nav--item">
+					<a href="/" class="nav--link">お問い合わせ</a>
+				</li>
+				<li class="nav--item">
+					<a href="/" class="nav--link">プライバシーポリシー</a>
+				</li>
+			</ul>
+		</nav>
 
-	<div class="footer_details">
-		<div class="row">
-			<div class="col">
-				<h3 class="company-name">株式会社アバンセシステム </h3>
-				<div class="address">
-					<span>本社</span>
-					<p>〒435-0037 <br class="sp">静岡県浜松市南区青屋町601-10　<br class="sp">TEL <a href="tel:053-411-8037">053-411-8037</a> / FAX 053-411-8038</p>
+		<div class="footer_details">
+			<div class="row">
+				<div class="col">
+					<h3 class="company-name">株式会社アバンセシステム </h3>
+					<div class="address">
+						<span>本社</span>
+						<p>〒435-0037 <br class="sp">静岡県浜松市南区青屋町601-10　<br class="sp">TEL <a href="tel:053-411-8037">053-411-8037</a> / FAX 053-411-8038</p>
+					</div>
+					<div class="address">
+						<span>東京営業所</span>
+						<p>〒164-8512 <br class="sp">東京都中野区中野4-1-1中野サンプラザ9F　<br class="sp">TEL <a href="tel:03-5942-3060">03-5942-3060</a></p>
+					</div>	
+					<div class="copy">
+						<small>Copyright Avance System Co.,Ltd.All rights reserved</small>
+					</div>
 				</div>
-				<div class="address">
-					<span>東京営業所</span>
-					<p>〒164-8512 <br class="sp">東京都中野区中野4-1-1中野サンプラザ9F　<br class="sp">TEL <a href="tel:03-5942-3060">03-5942-3060</a></p>
-				</div>	
-				<div class="copy">
-					<small>Copyright Avance System Co.,Ltd.All rights reserved</small>
+				<div class="col">
+					<figure>
+						<img src="/images/common/footer_img_1.png" alt="情報セキュリティ基本方針">
+					</figure>
+					<p>情報セキュリティ基本方針 </p>
 				</div>
-			</div>
-			<div class="col">
-				<figure>
-					<img src="/images/common/footer_img_1.png" alt="情報セキュリティ基本方針">
-				</figure>
-				<p>情報セキュリティ基本方針 </p>
 			</div>
 		</div>
 	</div>
-
 </div>
--- index.php
+++ index.php
@@ -39,79 +39,81 @@
 				</section>
 				<section class="sect_2">
 					<div class="wrapper">
-						<div class="row">
-							<div class="col">
-								<div class="c-header01">
-									<h3 class="c-header01__en">Service</h3>
-									<p class="c-header01__jp">サービス紹介</p>
+						<div class="container">
+							<div class="row">
+								<div class="col c-anim-up">
+									<div class="c-header01">
+										<h3 class="c-header01__en">Service</h3>
+										<p class="c-header01__jp">サービス紹介</p>
+									</div>
+									<p class="desc">
+										サービスを包括したテキストを入れたいです。<br class="pc">
+										サービスを包括したテキストを入れたいですサービスを包括したテキストを入れたいですサービスを包括したテキストを入れたいです。
+									</p>
 								</div>
-								<p class="desc">
-									サービスを包括したテキストを入れたいです。<br class="pc">
-									サービスを包括したテキストを入れたいですサービスを包括したテキストを入れたいですサービスを包括したテキストを入れたいです。
-								</p>
-							</div>
-							<div class="col">
-								<div class="card">
-									<picture >
-										<source srcset="/images/top/sect_2/sp/img1.jpg" media="(max-width: 899px)">
-										<img src="/images/top/sect_2/img1.jpg" alt="車載関連" class="">
-									</picture>
-									<div class="card--content">
-										<span class="card--content__num">01.</span>
-										<h3 class="card--content__title">車載関連</h3>
-										<p class="card--content__desc">車載ソフトウェア開発のご要望に豊富な実績とノウハウでお応えします</p>
+								<div class="col c-anim-up">
+									<div class="card">
+										<picture >
+											<source srcset="/images/top/sect_2/sp/img1.jpg" media="(max-width: 899px)">
+											<img src="/images/top/sect_2/img1.jpg" alt="車載関連" class="">
+										</picture>
+										<div class="card--content">
+											<span class="card--content__num">01.</span>
+											<h3 class="card--content__title">車載関連</h3>
+											<p class="card--content__desc">車載ソフトウェア開発のご要望に豊富な実績とノウハウでお応えし</p>
+										</div>
 									</div>
 								</div>
-							</div>
-							<div class="col">
-								<div class="card">
-									<picture >
-										<source srcset="/images/top/sect_2/sp/img2.jpg" media="(max-width: 899px)">
-										<img src="/images/top/sect_2/img2.jpg" alt="IoT関連" class="">
-									</picture>
-									<div class="card--content">
-										<span class="card--content__num">02.</span>
-										<h3 class="card--content__title">IoT関連</h3>
-										<p class="card--content__desc">テキストが入りますテキストが入りますテキストが入ります。</p>
+								<div class="col c-anim-up">
+									<div class="card">
+										<picture >
+											<source srcset="/images/top/sect_2/sp/img2.jpg" media="(max-width: 899px)">
+											<img src="/images/top/sect_2/img2.jpg" alt="IoT関連" class="">
+										</picture>
+										<div class="card--content">
+											<span class="card--content__num">02.</span>
+											<h3 class="card--content__title">IoT関連</h3>
+											<p class="card--content__desc">テキストが入りますテキストが入りますテキストが入ります。</p>
+										</div>
 									</div>
 								</div>
-							</div>
-							<div class="col">
-								<div class="card">
-									<picture >
-										<source srcset="/images/top/sect_2/sp/img3.jpg" media="(max-width: 899px)">
-										<img src="/images/top/sect_2/img3.jpg" alt="生産関連" class="">
-									</picture>
-									<div class="card--content">
-										<span class="card--content__num">03.</span>
-										<h3 class="card--content__title">生産関連</h3>
-										<p class="card--content__desc">テキストが入りますテキストが入りますテキストが入ります。</p>
+								<div class="col c-anim-up">
+									<div class="card">
+										<picture >
+											<source srcset="/images/top/sect_2/sp/img3.jpg" media="(max-width: 899px)">
+											<img src="/images/top/sect_2/img3.jpg" alt="生産関連" class="">
+										</picture>
+										<div class="card--content">
+											<span class="card--content__num">03.</span>
+											<h3 class="card--content__title">生産関連</h3>
+											<p class="card--content__desc">テキストが入りますテキストが入りますテキストが入ります。</p>
+										</div>
 									</div>
 								</div>
-							</div>
-							<div class="col">
-								<div class="card">
-									<picture >
-										<source srcset="/images/top/sect_2/sp/img4.jpg" media="(max-width: 899px)">
-										<img src="/images/top/sect_2/img4.jpg" alt="大学教育関連" class="">
-									</picture>
-									<div class="card--content">
-										<span class="card--content__num">04.</span>
-										<h3 class="card--content__title">大学教育関連</h3>
-										<p class="card--content__desc">テキストが入りますテキストが入りますテキストが入ります。</p>
+								<div class="col c-anim-up">
+									<div class="card">
+										<picture >
+											<source srcset="/images/top/sect_2/sp/img4.jpg" media="(max-width: 899px)">
+											<img src="/images/top/sect_2/img4.jpg" alt="大学教育関連" class="">
+										</picture>
+										<div class="card--content">
+											<span class="card--content__num">04.</span>
+											<h3 class="card--content__title">大学教育関連</h3>
+											<p class="card--content__desc">テキストが入りますテキストが入りますテキストが入ります。</p>
+										</div>
 									</div>
 								</div>
-							</div>
-							<div class="col">
-								<div class="card">
-									<picture >
-										<source srcset="/images/top/sect_2/sp/img5.jpg" media="(max-width: 899px)">
-										<img src="/images/top/sect_2/img5.jpg" alt="安否確認システム" class="">
-									</picture>
-									<div class="card--content">
-										<span class="card--content__num">05.</span>
-										<h3 class="card--content__title">安否確認システム</h3>
-										<p class="card--content__desc">テキストが入りますテキストが入りますテキストが入ります。</p>
+								<div class="col c-anim-up">
+									<div class="card">
+										<picture >
+											<source srcset="/images/top/sect_2/sp/img5.jpg" media="(max-width: 899px)">
+											<img src="/images/top/sect_2/img5.jpg" alt="安否確認システム" class="">
+										</picture>
+										<div class="card--content">
+											<span class="card--content__num">05.</span>
+											<h3 class="card--content__title">安否確認システム</h3>
+											<p class="card--content__desc">テキストが入りますテキストが入りますテキストが入ります。</p>
+										</div>
 									</div>
 								</div>
 							</div>
@@ -121,24 +123,26 @@
 			</div>
 			<section class="sect_3">
 				<div class="wrapper">
-					<div class="content">
-						<div class="row">
-							<div class="col">
-								<figure class="sp">
-									<img src="/images/top/sect_3/sp/img_1.jpg" alt="">
-								</figure>
-							</div>
-							<div class="col">
-								<div class="c-header01">
-									<h3 class="c-header01__en">Company</h3>
-									<p class="c-header01__jp">会社案内</p>
+					<div class="container">
+						<div class="content">
+							<div class="row">
+								<div class="col">
+									<figure class="sp">
+										<img src="/images/top/sect_3/sp/img_1.jpg" alt="">
+									</figure>
 								</div>
-								<p class="desc">
-								テキストが入りますテキストが入りますテキストが入ります。<br>
-								テキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入ります。<br>
-								テキストが入りますテキストが入りますテキストが入りますテキスト。<br>
-								</p>
-								<a href="" class="c-button01 c-button01--primary c-button01--lg">会社案内</a>
+								<div class="col">
+									<div class="c-header01">
+										<h3 class="c-header01__en c-anim-up">Company</h3>
+										<p class="c-header01__jp c-anim-up">会社案内</p>
+									</div>
+									<p class="desc c-anim-up">
+									テキストが入りますテキストが入りますテキストが入ります。<br>
+									テキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入ります。<br>
+									テキストが入りますテキストが入りますテキストが入りますテキスト。<br>
+									</p>
+									<a href="" class="c-button01 c-button01--primary c-button01--lg"><span class="c-button01--text">会社案内</span></a>
+								</div>
 							</div>
 						</div>
 					</div>
@@ -146,86 +150,94 @@
 			</section>
 			<section class="sect_4">
 				<div class="wrapper">
-					<figure class="sp">
-						<img src="/images/top/sect_4/sp/bg_img.jpg" alt="">
-					</figure>
-					<div class="content">
-						<div class="c-header01">
-							<h3 class="c-header01__en">Avance.Lab</h3>
-							<p class="c-header01__jp">アバンセ ラボ</p>
+					<div class="container">
+						<div class="bg">
+							<figure class="sp">
+								<img src="/images/top/sect_4/sp/bg_img.jpg" alt="">
+							</figure>
+							<div class="content">
+								<div class="c-header01">
+									<h3 class="c-header01__en c-anim-up">Avance.Lab</h3>
+									<p class="c-header01__jp c-anim-up">アバンセ ラボ</p>
+								</div>
+								<p class="desc c-anim-up">
+								テキストが入りますテキストが入りますテキストが入ります。<br>
+								テキストが入りますテキストが入りますテキスト<br class="pc">
+								テキストが入りますテキストが入りますテキストが入りますテキスト。
+								</p>
+								<a href="" class="c-button01 c-button01--secondary c-button01--lg"><span class="c-button01--text">採用情報</span></a>
+							</div>
 						</div>
-						<p class="desc">
-						テキストが入りますテキストが入りますテキストが入ります。<br>
-						テキストが入りますテキストが入りますテキスト<br class="pc">
-						テキストが入りますテキストが入りますテキストが入りますテキスト。
-						</p>
-						<a href="" class="c-button01 c-button01--secondary c-button01--lg">採用情報</a>
 					</div>
 				</div>
 			</section>
 			<section class="sect_5">
 				<div class="wrapper">
-					<div class="bg_cont">
-						<div class="bg_cont--content">
-							<h3 class="bg_cont--content__title">向上心を持って、一流のSEとして<br>私たちと共に働きませんか？</h3>
-							<a href="" class="c-button01 c-button01--secondary c-button01--lg">採用情報</a>
+					<div class="container">
+						<div class="bg_cont">
+							<div class="bg_cont--content">
+								<h3 class="bg_cont--content__title c-anim-up">向上心を持って、一流のSEとして<br>私たちと共に働きませんか？</h3>
+								<a href="" class="c-button01 c-button01--secondary c-button01--lg"><span class="c-button01--text">採用情報</span></a>
+							</div>
 						</div>
 					</div>
 				</div>
 			</section>
 			<section class="sect_6">
 				<div class="wrapper">
-					<div class="c-header01">
-						<h3 class="c-header01__en">News</h3>
-						<p class="c-header01__jp">新着情報</p>
+					<div class="container">
+						<div class="c-header01">
+							<h3 class="c-header01__en c-anim-up">News</h3>
+							<p class="c-header01__jp c-anim-up">新着情報</p>
+						</div>
+						<ul class="news">
+							<li class="news__item">
+								<a href="" class="news__link">
+									<figure class="news__img">
+										<img src="/images/top/sect_6/img1.jpg" alt="タイトルが入りますタイトルが入ります">
+									</figure>
+									<div class="news__content">
+										<p class="news__content__wrap"><span class="news__content--date">2021.01.01</span><span class="news__content__wrap--separator">&nbsp;&nbsp;:&nbsp;&nbsp;</span><span class="news__content--cat">お知らせ</span></p>
+										<h4 class="news__content--title">タイトルが入りますタイトルが入ります</h4>
+									</div>
+								</a>
+							</li>
+							<li class="news__item">
+								<a href="" class="news__link">
+									<figure class="news__img">
+										<img src="/images/top/sect_6/img2.jpg" alt="タイトルが入りますタイトルが入ります">
+									</figure>
+									<div class="news__content">
+										<p class="news__content__wrap"><span class="news__content--date">2021.01.01</span><span class="news__content__wrap--separator">&nbsp;&nbsp;:&nbsp;&nbsp;</span><span class="news__content--cat">休業のお知らせ</span></p>
+										<h4 class="news__content--title">タイトルが入りますタイトルが入ります</h4>
+									</div>
+								</a>
+							</li>
+							<li class="news__item">
+								<a href="" class="news__link">
+									<figure class="news__img">
+										<img src="/images/top/sect_6/img3.jpg" alt="タイトルが入りますタイトルが入ります">
+									</figure>
+									<div class="news__content">
+										<p class="news__content__wrap"><span class="news__content--date">2021.01.01</span><span class="news__content__wrap--separator">&nbsp;&nbsp;:&nbsp;&nbsp;</span><span class="news__content--cat">お知らせ</span></p>
+										<h4 class="news__content--title">タイトルが入りますタイトルが入ります</h4>
+									</div>
+								</a>
+							</li>
+							<li class="news__item">
+								<a href="" class="news__link">
+									<figure class="news__img">
+										<img src="/images/top/sect_6/img4.jpg" alt="タイトルが入りますタイトルが入ります">
+									</figure>
+									<div class="news__content">
+										<p class="news__content__wrap"><span class="news__content--date">2021.01.01</span><span class="news__content__wrap--separator">&nbsp;&nbsp;:&nbsp;&nbsp;</span><span class="news__content--cat">お知らせ</span></p>
+										<h4 class="news__content--title">タイトルが入りますタイトルが入ります</h4>
+									</div>
+								</a>
+							</li>
+						</ul>
+						<a href="" class="c-button01 c-button01--primary c-button01--sm"><span class="c-button01--text">一覧</span></a>
 					</div>
-					<ul class="news">
-						<li class="news__item">
-							<a href="" class="news__link">
-								<figure class="news__img">
-									<img src="/images/top/sect_6/img1.jpg" alt="タイトルが入りますタイトルが入ります">
-								</figure>
-								<div class="news__content">
-									<p class="news__content__wrap"><span class="news__content--date">2021.01.01</span><span class="news__content__wrap--separator">&nbsp;&nbsp;:&nbsp;&nbsp;</span><span class="news__content--cat">お知らせ</span></p>
-									<h4 class="news__content--title">タイトルが入りますタイトルが入ります</h4>
-								</div>
-							</a>
-						</li>
-						<li class="news__item">
-							<a href="" class="news__link">
-								<figure class="news__img">
-									<img src="/images/top/sect_6/img2.jpg" alt="タイトルが入りますタイトルが入ります">
-								</figure>
-								<div class="news__content">
-									<p class="news__content__wrap"><span class="news__content--date">2021.01.01</span><span class="news__content__wrap--separator">&nbsp;&nbsp;:&nbsp;&nbsp;</span><span class="news__content--cat">休業のお知らせ</span></p>
-									<h4 class="news__content--title">タイトルが入りますタイトルが入ります</h4>
-								</div>
-							</a>
-						</li>
-						<li class="news__item">
-							<a href="" class="news__link">
-								<figure class="news__img">
-									<img src="/images/top/sect_6/img3.jpg" alt="タイトルが入りますタイトルが入ります">
-								</figure>
-								<div class="news__content">
-									<p class="news__content__wrap"><span class="news__content--date">2021.01.01</span><span class="news__content__wrap--separator">&nbsp;&nbsp;:&nbsp;&nbsp;</span><span class="news__content--cat">お知らせ</span></p>
-									<h4 class="news__content--title">タイトルが入りますタイトルが入ります</h4>
-								</div>
-							</a>
-						</li>
-						<li class="news__item">
-							<a href="" class="news__link">
-								<figure class="news__img">
-									<img src="/images/top/sect_6/img4.jpg" alt="タイトルが入りますタイトルが入ります">
-								</figure>
-								<div class="news__content">
-									<p class="news__content__wrap"><span class="news__content--date">2021.01.01</span><span class="news__content__wrap--separator">&nbsp;&nbsp;:&nbsp;&nbsp;</span><span class="news__content--cat">お知らせ</span></p>
-									<h4 class="news__content--title">タイトルが入りますタイトルが入ります</h4>
-								</div>
-							</a>
-						</li>
-					</ul>
-					<a href="" class="c-button01 c-button01--primary c-button01--sm">一覧</a>
 				</div>
 			</section>
 		</main>
@@ -235,6 +247,36 @@
 		</footer>
 	</div><!-- //AllBox -->
 	<?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/footer_tag.php"); ?>
+
+	<script>
+		const secth2 = document.querySelector('.main_content .sect_1 .content h2');
+		const secth3 = document.querySelector('.main_content .sect_1 .content h3');
+		window.addEventListener('load', function() {
+			secth2.classList.add('isOpened');
+			secth3.classList.add('isOpened');
+		});
+		setTimeout( function () {
+			secth2.classList.add('isOpened');
+			secth3.classList.add('isOpened');
+		}, 2000);
+
+		$(function () {
+			$(window).on("scroll load", function () {
+				var t = $(this).scrollTop();
+				var sect3pos = $('.sect_3').offset().top;
+				var sect6pos = $('.sect_6').offset().top;
+				var sect3_new_pos = sect3pos - t;
+				var sect6_new_pos = sect6pos - t;
+				console.log(sect3_new_pos)
+				if (sect3_new_pos < 500) {
+					$('.sect_3 .wrapper .content .row .col:first-child').addClass('showAnim');
+				}
+				if (sect6_new_pos < 400) {
+					$('.sect_6 .wrapper .news .news__item').addClass('showAnim');
+				}
+			});
+		});
+	</script>
 </body>
 
 </html>
